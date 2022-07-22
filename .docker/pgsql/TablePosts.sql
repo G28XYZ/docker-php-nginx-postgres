a653163983b6f467708cbe59758d40bddb312235
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-07-22 16:00:12 +0700

    feat: add php.ini env Dockerfile for pgsql

---
+++ .docker/pgsql/TablePosts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE posts (
+    id SERIAL,
+    user_post_id INT,
+    author VARCHAR(255),
+    content TEXT
+);
---
+++ .docker/pgsql/TableUsers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id SERIAL,
+    full_name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
